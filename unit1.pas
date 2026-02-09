13277b15d34abbeb3473a4e8c31d569fecacf99d
Hevanafa <dhevansafa4@gmail.com>
2026-02-09 11:29:16 +0700

    added TTween field declarations

--- unit1.pas
+++ unit1.pas
@@ -11,11 +11,42 @@ uses
   Controls, Graphics, Dialogs;
 
 type
-  TEasingFunction = function(currentTime, startValue, delta, duration: single): single;
+  TEasingFunction = function(currentTime, startVal, deltaVal, duration: single): single;
   TTweenCallback = procedure(t: TTweenCallback) of object;
 
   TTween = class
+  private
+    fStartValue: single;
+    fEndValue: single;
+    fCurrentValue: single;
+    fDuration: single;
+    fElapsed: single;
+    fEasing: TEasingFunction;
+    fIsPlaying: boolean;
+    fIsComplete: boolean;
 
+    { Callbacks }
+    fOnStart: TTweenCallback;
+    fOnUpdate: TTweenCallback;
+    fOnComplete: TTweenCallback;
+
+  public
+    constructor create(startVal, endVal, duration: single);
+    destructor destroy; override;
+
+    procedure update(deltaTime: double);
+    procedure play;
+    procedure pause;
+    procedure reset;
+
+    function setEasing(easing: TEasingFunction): TTween;
+    function onStart(callback: TTweenCallback): TTween;
+    function onUpdate(callback: TTweenCallback): TTween;
+    function onComplete(callback: TTweenCallback): TTween;
+    function thenTween(nextTween: TTween): TTween;
+
+    property currentValue: single read fCurrentValue;
+    property isComplete: boolean read fIsComplete;
   end;
 
   TForm1 = class(TForm)
